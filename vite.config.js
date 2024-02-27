b0622b3190471fe38f62e4da60474da70812683e
fabrciovvieira <fabriciovvieira@gmail.com>
2024-02-27 19:33:46 -0300

    config para deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/myportfolio/',
+  base: '/myportfolio',
   plugins: [reactRefresh()],
-
 });
 
