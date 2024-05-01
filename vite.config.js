885dc6054213c5499ca23befff9b2e57a931932a
fabrciovvieira <fabriciovvieira@gmail.com>
2024-05-01 20:11:56 -0300

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/myportfolio',
-  plugins: [reactRefresh()],
-});
-
